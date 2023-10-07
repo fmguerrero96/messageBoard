17f0f8269bbea272a562ddeb0a60c693637a689f
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-07 19:26:59 -0400

    Create a messages array containing some sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "I'd like to say hi",
+    user: "SciFly",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
